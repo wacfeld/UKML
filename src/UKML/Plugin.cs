e7b3db8ab12ff354d1cb51f02fe45e532a06dbcb
Daniel Yu <wacfeldwang333@gmail.com>
2025-04-09 07:12:46 -0400

    v0.1.0

--- src/UKML/Plugin.cs
+++ src/UKML/Plugin.cs
@@ -9,7 +9,7 @@ public class Plugin : BaseUnityPlugin
 {
     public const string PLUGIN_GUID = "wacfeld.ukml";
     public const string PLUGIN_NAME = "ULTRAKILL Mustn't Live";
-    public const string PLUGIN_VERSION = "0.0.0";
+    public const string PLUGIN_VERSION = "0.1.0";
 
     /// <summary> We need to have an instance of this in order to do patches </summary>
     readonly Harmony harmony = new(PLUGIN_GUID);
