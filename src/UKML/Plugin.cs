ce71b1c2c997866be10cd403c00a0e7204501852
daniel yu <wacfeldwang333@gmail.com>
2025-04-10 10:51:55 -0400

    formatting/comments

--- src/UKML/Plugin.cs
+++ src/UKML/Plugin.cs
@@ -12,9 +12,9 @@ public class Plugin : BaseUnityPlugin
     public const string PLUGIN_NAME = "ULTRAKILL Mustn't Live";
     public const string PLUGIN_VERSION = "0.1.0";
 
-    /// <summary> We need to have an instance of this in order to do patches </summary>
     readonly Harmony harmony = new(PLUGIN_GUID);
     
+    // TODO stop game from muting non-error messages at the start, and also figure out how to log messages from inside patches
     public ManualLogSource Log => Logger;
 
     private void Awake()
@@ -22,16 +22,13 @@ public class Plugin : BaseUnityPlugin
         harmony.PatchAll();
         Log.LogInfo($"Loaded {PLUGIN_NAME}");
     }
-
-
-
 }
 
 [HarmonyPatch(typeof(NewMovement))]
 [HarmonyPatch("GetHurt")]
 class PatchGetHurt
 {
-    // TODO make this only apply to difficulty 6
+    // TODO make this only apply to difficulty 5
     // set hardDamageMultiplier to 1
     static void Prefix(ref float hardDamageMultiplier)
     {
